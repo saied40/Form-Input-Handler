7c3befe9f1e3c025baf35ccaf4eea420c7992d84
naoh <noahshaheen4@gmail.com>
2024-04-19 07:43:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const handleChange = (e) => {
+    document.querySelector('.'+e.target.name).innerText = e.target.value;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let temp = e.target.children[0].children;
+    alert(`First Name: ${temp[0].value}\nLast Name: ${temp[1].value}\nEmail: ${temp[2].value}`);
+    [...temp].map(ele => ele.value = "");
+    [...e.target.children[1].children].map(ele => ele.innerText = "");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="inputs">
+        <input
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="E-mail"
+          name="email"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="outputs">
+        <p className="firstName">first-name</p>
+        <p className="lastName">last-name</p>
+        <p className="email">email</p>
+      </div>
+      <input type="submit" />
+    </form>
   );
-}
-
-export default App;
+};
